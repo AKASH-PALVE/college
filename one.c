77fd6c055b8bf2a838c80ec531acd593d37cfc05
AKASH-PALVE <108952229+AKASH-PALVE@users.noreply.github.com>
2022-09-30 15:50:36 +0530

    Add files via upload

---
+++ one.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+     // calculator
+     
+     int a,b;
+     printf("Enter a and b :\n");
+     scanf("%d%d ",&a,&b);
+     printf("addition :%d\n", a+b);
+     printf("substrction :%d\n", a-b);
+     printf("Multiplication :%d\n", a*b);
+     printf("Division :%d\n", a/b);
+     
+     
+    
+
+    return 0;
+}
+
